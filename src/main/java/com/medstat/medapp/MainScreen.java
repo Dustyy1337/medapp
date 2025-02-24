324eef746583f02eef4676921fc77ff9749ef944
Dustyy1337 <mchlmarczak@gmail.com>
2025-02-24 21:06:07 +0100

    MySQL connection added

--- src/main/java/com/medstat/medapp/MainScreen.java
+++ src/main/java/com/medstat/medapp/MainScreen.java
@@ -2,14 +2,23 @@ package com.medstat.medapp;
 
 import com.medstat.model.User;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class MainScreen {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @GetMapping("/")
     public String mainScreen(HttpSession session){
         if(session.getAttribute("username")==null) return "redirect:/login";
@@ -24,14 +33,20 @@ public class MainScreen {
 
     @PostMapping("/login")
     public String loginAuth(@ModelAttribute("user") User user, Model model, HttpSession session) {
-        //login auth
-
+        String username = user.getUsername();
+        String password = user.getPassword();
+        String sql = "SELECT users.username AS username, users.firstname AS firstname, users.lastname AS lastname, usertypes.usertype AS usertype FROM users INNER JOIN usertypes ON users.usertype_id = usertypes.usertype_id WHERE users.username = ? AND users.password = ?";
+        List<Map<String, Object>> resultSet = jdbcTemplate.queryForList(sql, username, password);
+        if(resultSet.isEmpty()) return "redirect:/login";
         session.setAttribute("username", user.getUsername());
+        session.setAttribute("firstname", resultSet.get(0).get("firstname"));
+        session.setAttribute("lastname", resultSet.get(0).get("lastname"));
+        session.setAttribute("usertype", resultSet.get(0).get("usertype"));
         return "redirect:/dashboard";
     }
     @GetMapping("/dashboard")
     public String dashboard(Model model, HttpSession session) {
-        if(session.isNew()) return "redirect:/login";
+        if(session.getAttribute("username")==null) return "redirect:/login";
         return "dashboard";
     }
 }
